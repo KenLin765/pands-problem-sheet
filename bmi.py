c709cb5eec3471c67fdc059cdc0029b40a9c827f
KenLin765 <98061901+KenLin765@users.noreply.github.com>
2022-01-31 20:44:40 +0000

    pands-problem-sheet

---
+++ bmi.py
@@ -0,0 +1,14 @@
+## Solution to weekly task 02
+
+##Idea for code from website - https://github.com/mindninjaX/Python-Projects-for-Beginners/blob/master/BMI%20Calculator/BMI%20Calculator.py
+
+height = float(input("Enter height(cm)"))
+weight = float(input("Enter weight(kg)"))
+
+##float converts number into number with decimal point
+
+BMI = weight / (height/100)**2
+
+print(f"The BMI is (kg/m2) {BMI}")
+
+##print BMI
